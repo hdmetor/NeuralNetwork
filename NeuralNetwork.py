fixed backprop for multiple examples; added costs function; bug: weights and biases not updating

NeuralNetwork.py
@@ -11,8 +11,8 @@ def sgm(x, der = False):
 class NeuralNetwork:
 
     def __init__(self, shape, activation = sgm):
-        """Create  a new Neural Network with a given shape"""
-        # shape = [input, hidden1, ..., hiddenk, output]
+        """Create  a new Neural Network with a given shape
+        shape = [input, hidden1, ..., hiddenk, output]"""
         self.shape = shape
         self.activation = activation
         # init weights
@@ -27,20 +27,29 @@ class NeuralNetwork:
         # we divide each 'column' for its max
         # should we scale the data in another way? (x-std)/mean
 
-        #insert a sanity check to make sure that the input (and target) passed matches the expected dimensions
         # make sure that if the input is already a np array it will not wrapped again
-        self.input = np.array(input) / np.amax(input, axis = 0)
-        self.target = np.array(target) / np.amax(target)
-        #self.cost = sum((self.feed_forward(input) - target))
+        self.input = (np.array(input) / np.amax(input, axis = 0)).T
+        self.target = (np.array(target) / np.amax(target)).T
 
-    def feed_forward(self, input, weights = None):
+        if self.input.shape[0] != self.shape[0]:
+            print('Input and shape of the network not compatible')
+            exit()
+
+        if self.target.shape[0] != self.shape[-1]:
+            print('Output and shape of the network not compatible')
+            exit()
+
+    def feed_forward(self, input = None, weights = None):
         """Pass the input to the network """
         if weights == None:
             weights = self.weights
 
-        # is this correct?
-        self.output.append(input)
-        result = input.T
+        if input == None:
+            input = self.input
+
+        result = input
+        self.output.append(result)
+
 
 
         for index, w  in enumerate(weights):
@@ -72,13 +81,13 @@ class NeuralNetwork:
         for l in range(steps, 0, -1):
             delta = np.multiply(
                         np.dot(self.weights[l].T,self.delta[steps-l]),
-                        self.activation(self.output[l-1], der = True)
+                        self.activation(self.output[l], der = True)
                         )
             self.delta.append(delta)
 
         # delta[i] contains the delta for level i
         self.delta.reverse()
-        
+
 
     def update_weights(self,  eta = 0.3):
         total = 1
@@ -88,10 +97,12 @@ class NeuralNetwork:
     def update_biases(self, eta = 0.3):
         # this is going to be the number of the training examples
         total = 1
-        return [self.biases[i] - (eta/total)* self.delta[i] for i in range(len(self.biases))] 
-        
+        return [self.biases[i] - (eta/total)* self.delta[i] for i in range(len(self.biases))]
 
 
+    def cost(self):
+        """Calculate the cost function using the current weights and output"""
+        return np.linalg.norm(self.activation(self.output[-1]) - self.target) ** 2
 
 if __name__ == '__main__':
 
@@ -101,18 +112,18 @@ if __name__ == '__main__':
 
     X, y  = [[1,2]], [3]
 
+    X, y = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[2,3,4,5],[2,3,4,5]], [[10,11],[10,11],[10,11], [22,23], [23,25]]
 
 
-    NN = NeuralNetwork([2, 3, 1])
+    NN = NeuralNetwork([4, 6,7,10,3, 2])
     NN.init(X,y)
 
-    #print(NN.input)
-    #print(NN.target)
 
-    #print("\nresult is")
-    #print(NN.feed_forward(np.array(X)))
-
-    NN.feed_forward(np.array(X))
+    NN.feed_forward()
+    c1 = NN.cost()
+    print("Cost ", c1)
     NN.back_propagation()
     NN.update_biases()
     NN.update_weights()
+    NN.feed_forward()
+    print("Cost ", NN.cost())