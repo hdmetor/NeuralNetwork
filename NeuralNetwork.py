804f9464320615b25184bcbdd0b2131de2022401
hdmetor <hdmetor@gmail.com>
2015-05-13 19:33:09 -0400

    Code refactoring / cleaning

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pprint as pp
 
 def sgm(x, der = False):
     """logistic sigmoid function"""
@@ -10,6 +11,15 @@ def sgm(x, der = False):
 
 class NeuralNetwork:
 
+    def init_weights(self, shape):
+        self.weights = [np.random.randn(j , i) for i, j in zip(shape[:-1], shape[1:])]
+
+    def init_biases(self, shape):
+        self.biases = [np.random.randn( i, 1 ) for i in self.shape[1:]]
+        
+    def init_output(self):
+        self.output = []
+
     def __init__(self, shape, activation = sgm):
         """Create  a new Neural Network with a given shape
         shape = [input, hidden1, ..., hiddenk, output]"""
@@ -17,10 +27,9 @@ class NeuralNetwork:
         self.activation = activation
         # init weights
         # weight is an array that contains at position i the weight for level i-i to i
-        # we added a column to take into account biases
-        self.weights = [np.random.randn(j , i) for i, j in zip(self.shape[:-1], self.shape[1:])]
-        self.biases = [np.random.randn( i, 1 ) for i in self.shape[1:]]
-        self.output = []
+        self.init_weights(self.shape)
+        self.init_biases(self.shape)
+        self.init_output()
 
 
     def feed_forward(self, input = None, weights = None):
@@ -37,7 +46,10 @@ class NeuralNetwork:
         for index, w  in enumerate(weights):
         # each position in weight list represent a level in the network
 
-            # caluclates the output of the nodes
+            # calculates the output of the nodes
+            print(index)
+            print(w.shape)
+            print(result.shape)
             level_output = np.dot(w, result) + self.biases[index]
             self.output.append(level_output)
             result = self.activation(level_output)
@@ -74,37 +86,37 @@ class NeuralNetwork:
         #print("O",[w.shape for w in self.output])
 
     def gradient_descend(self, eta):
-
-        
         total = len(self.input) 
-
         self.update_weights(total, eta)
         self.update_biases(total, eta)
-        
-        
         
     def update_weights(self, total, eta):
         """Use backpropagation to update weights"""
         self.weights =  [self.weights[i] - (eta/total) * np.dot(self.delta[i], self.output[i].T) for i in range(len(self.delta))]
 
-
     def update_biases(self, total, eta):
         """Use backpropagation to update the biases"""
         self.biases =  [self.biases[i] - (eta/total)* self.delta[i].sum(axis=1)[:,None] for i in range(len(self.biases))]
-
 
     def cost(self):
         """Calculate the cost function using the current weights and output"""
         return np.linalg.norm(self.activation(self.output[-1]) - self.target) ** 2
 
-    def SGD(input, batch_size, epochs = 20, eta = .3):
+    def SGD(self,input, target, batch_size, epochs = 20, eta = .3):
 
+        # maybe remove this in the future?
+        if isinstance(input, list):
+            input = np.array(input)
+        if isinstance(target, list):
+            target = np.array(target)
+
+        self.input = input.T
+        self.target = target.T
 
         # sanity / shape checks that input / output respect the desired dimensions
         if self.input.shape[0] != self.shape[0]:
             print('Input and shape of the network not compatible')
             exit()
-
         if self.target.shape[0] != self.shape[-1]:
             print('Output and shape of the network not compatible')
             exit()
@@ -114,19 +126,26 @@ class NeuralNetwork:
         #self.target = (np.array(target) / np.amax(target)).T
 
         total = len(input)
-
         diff = total % batch_size
+        print(total, diff)
+        # we discard the last examples for now
         if diff != 0:
             input = input[: total - diff]
             total = len(input)
 
-        
+        if epochs > total:
+            # this is only for debug mode
+            print("N", total)
+            epochs = total
         for epoch in range(epochs):
+            print("A",range(epochs))
             # each time shuffle the data
-            np.array.shuffle(input)
+            np.random.shuffle(input)
             # create a list of batches
             batches = [input[k:k+batch_size] for k in range(0,total,batch_size)]
             for batch in batches:
+                print('bath')
+                pp.pprint(batch)
                 # pass the input trought the newtork
                 self.feed_forward(batch)
                 # calcualte delta for all levels
@@ -144,22 +163,25 @@ if __name__ == '__main__':
 
     X, y  = [[1,2]], [3]
 
-    X, y = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[2,3,4,5],[2,3,4,5]], [[10,11],[10,11],[10,11], [22,23], [23,25]]
+    X, y = [[1,2,3,4],[11,22,33,44],[21,22,32,24],[2,3,4,5],[2,3,4,5]], [[10,11],[10,11],[10,11], [22,23], [23,25]]
 
 
     #NN = NeuralNetwork([4, 6,7,10,3, 2])
     NN = NeuralNetwork([4, 6, 2])
+    """
     NN.init(X,y)
 
 
     NN.feed_forward()
     NN.back_propagation()
     NN.gradient_descend(eta = .3)
+"""
 
-    c1 = NN.cost()
-    NN.feed_forward()
-    c2 = NN.cost()
-    print('is cost decreasing? ',c2 < c1)
+    NN.SGD(X,y,1)
+    #c1 = NN.cost()
+    #NN.feed_forward()
+    #c2 = NN.cost()
+    #print('is cost decreasing? ',c2 < c1)
 
 """
 TODOS:
