f6546276e0158b4d4a7b3af13dfc4e4efc10afca
hdmetor <hdmetor@gmail.com>
2015-10-18 18:35:01 -0700

    Better docstrings, cleaner code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,7 +3,8 @@ import pprint as pp
 import math
 
 def sgm(x, der=False):
-    """logistic sigmoid function"""
+    """Logistic sigmoid function.
+    Use der=True for the derivative."""
     if not der:
         return 1 / (1 + np.exp(-x))
     else:
@@ -11,6 +12,13 @@ def sgm(x, der=False):
         return simple * (1 - simple)
 
 class NeuralNetwork:
+    """Neural Network class.
+
+    Args:
+        shape (list): shape of the network. First element is the input layer, last element
+        is the ouput layer.
+        activation (optional): pass the activation function. Defaults to sigmoid. 
+        """
 
     def _init_weights(self, shape):
         self.weights = [np.random.randn(j , i) for i, j in zip(shape[:-1], shape[1:])]
@@ -22,42 +30,21 @@ class NeuralNetwork:
         self.output = []
 
     def __init__(self, shape, activation=sgm):
-        """Create  a new Neural Network with a given shape
-        shape = [input, hidden_1, ..., hidden_k, output]
-        using the function activation"""
         self.shape = shape
         self.activation = activation
-        # init weights
-        # weight is an array that contains at position i the weight for level i-i to i
         self._init_weights(self.shape)
         self._init_biases(self.shape)
         self._init_output()
-        print (" weight init ")
-        for w in self.weights:
-            print(w.shape)
 
-    # TODO = remove weights
     def feed_forward(self, input):
         """Given the input and, return the predicted value according to the current weights."""
 
         result = input
         self.output.append(result)
-        #print("input" , input)
-        #print("weight", weights)
 
-        # TODO = use zip
         for w, b  in zip(self.weights, self.biases):
-        # each position in weight list represent a level in the network
-
-            # calculates the output of the nodes
-            # print("\t inside feed forward, level ", index)
-            # print("w shape: ", w.shape)
-            # print("biases shape: ", self.biases[index].shape)
-            # print("result shape:", result.shape)
-
             result = self.activation(np.dot(w, result) + b)
             self.output.append(result)
-            # result = self.activation(level_output)
 
         # the last level is the output
         return result
@@ -66,7 +53,6 @@ class NeuralNetwork:
         """ Given the input and the output (typically from a batch),
         it calculates the corresponding deltas.
         """
-        # TODO = should delta be a variable in the batch cycle?
         # delta for the back propagation
         self.delta = []
 
@@ -92,21 +78,8 @@ class NeuralNetwork:
         # delta[i] contains the delta for layer i+1
         self.delta.reverse()
 
-        #print("W",[w.shape for w in self.weights])
-        #print("B",[w.shape for w in self.biases])
-        #print("D",[w.shape for w in self.delta])
-        #print("O",[w.shape for w in self.output])
-
-
     def update_weights(self, total, eta):
         """Use backpropagation to update weights"""
-        # for (i, d) in enumerate(self.delta):
-        #     print("IN UW it ", i)
-        #     print("weights shape: ", self.weights[i].shape)
-        #     print("delta shape: ",d.shape)
-        #     print("output shape.t: ",self.output[i].T.shape)
-
-        #     self.weights[i] -= (eta / total) * np.dot(d, self.output[i].T)
         self.weights =  [self.weights[i] - (eta/total) * np.dot(self.delta[i], self.output[i].T) for i, e in enumerate(self.delta)]
 
     def update_biases(self, total, eta):
@@ -147,51 +120,42 @@ class NeuralNetwork:
             self.input = self.input[: total - diff]
             self.target = self.target[: total - diff]
             total = self.input.shape[1]
-        print("epochs ", epochs, total, )
-        if epochs > total:
-            # this is only for debug mode
-            epochs = total
-        print("epochs ", epochs)
+        
         for epoch in range(epochs):
             # for each epoch, we reshuffle the data and train the network
-            print("*****Beginning of epoch:", epoch)
+            print("***** Epoch:", epoch)
             # TODO = each time shuffle the data
             #p = np.random.permutation(len(input))
             #self.input = self.input[p]
             #self.target = self.target[p]
             # create a list of batches (input and target)
-            batchesInput = [self.input[:, k:k + batch_size] for k in range(0, total, batch_size)]
-            batchesTarget = [self.target[:, k:k + batch_size] for k in range(0, total, batch_size)]
-            for batchInput, batchTarget in zip(batchesInput, batchesTarget):
+            batches_input = [self.input[:, k:k + batch_size] for k in range(0, total, batch_size)]
+            batches_target = [self.target[:, k:k + batch_size] for k in range(0, total, batch_size)]
+            for batch_input, batch_target in zip(batches_input, batches_target):
                 
+                # reset the status of the internal variables each time
                 self._init_output()
                 
-                # print('batch input:')
-                # pp.pprint(batchInput)
-                # print('batch target:')
-                # pp.pprint(batchTarget)
-                # pass the input trought the newtork
-                # print("feeding forward")
-                self.feed_forward(batchInput)
+                # calulating the values corresponding to the inputs
+                self.feed_forward(batch_input)
+                
+                # do backpropagation
                 # calcualte delta for all levels
-                # print("calculating deltas")
-                self.calculate_deltas(batchInput, batchTarget)
-                # updating weights and biases
-                # print("updating weights")
+                self.calculate_deltas(batch_input, batch_target)
+                # update internal variables
                 self.update_weights(batch_size, eta)
                 self.update_biases(batch_size, eta)
-                if (print_cost):
-                    print("Error is ", self.cost(1, 2))
+            
+            if (print_cost):
+                print("Error is ",
+                    self.cost(self.feed_forward(self.input), self.target)
+                    )
 
 
     def predict(self, data):
-        result = np.array(data).T
-        #output = np.array(output).T
-        for w, b in zip(self.weights, self.biases):
-        
-            result = np.dot(w, result) + b
-        return result.T
-        #print("cost was ", self.cost(result, output))
+        if isinstance(data, list):
+            data = np.array(data).T
+        return self.feed_forward(data)
 
 
 if __name__ == '__main__':
@@ -216,26 +180,18 @@ if __name__ == '__main__':
     #     X_test.append([(i+1)/m])
     #     y_test.append([((i+1)/m) ** 2])
 
-    for xx in np.linspace(-1, 1, 1000):
-        for yy in np.linspace(-1, 1, 1000):
+    # for xx in np.linspace(-1, 1, 1000):
+    #     for yy in np.linspace(-1, 1, 1000):
             
-            X.append([xx, yy])
-            y.append([np.sin(xx - yy) + np.cos(xx + yy)])
+    #         X.append([xx, yy])
+    #         y.append([np.sin(xx - yy) + np.cos(xx + yy)])
 
-
+    for xx in np.linspace(-10, 10, 10000):
+        X.append([xx])
+        y.append([xx ** 2])
     #NN = NeuralNetwork([4, 6,7,10,3, 2])
-    NN = NeuralNetwork([2, 4, 1])
+    NN = NeuralNetwork([1, 2, 1])
 
     print("starting sgd")
-    NN.SGD(X,y,100, epochs=4)
-    #NN.test(X_test, y_test)
-    print(NN.predict([[0,0]]))
-
-
-
-"""
-TODOS:
-
-- insert a save and load method
-
-"""
+    NN.SGD(X,y,100, epochs=4, print_cost=True)
+    print(NN.predict([[0], [1], [-11]]))
